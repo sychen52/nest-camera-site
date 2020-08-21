bb27d8b438098bbf5748a9bde4770508d8b14526
sychen52 <sychen52@gmail.com>
2020-08-20 19:49:11 -0700

    add additional log
    make the webpage longer

--- capture.js
+++ capture.js
@@ -56,6 +56,7 @@ function getImage(config, fq){
         else {
             console.log(now, response.status);
             if (response.status != 404) {
+                console.log(response)
                 connect(config, fq);
             }
             else {
